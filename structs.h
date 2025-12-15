e77d510721c55c4d3cb5e4724a4d4cbcd2820c48
Uyen <25730092@ms.uit.edu.vn>
2025-12-15 21:54:53 +0700

    Dinh nghia cac hang so game

--- structs.h
+++ structs.h
@@ -7,3 +7,12 @@
  * - Tao struct Piece va GameState
  * - Dinh nghia 7 hinh dang khoi Tetromino
 */
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+const int BOARD_WIDTH = 10; 
+const int BOARD_HEIGHT = 20;
+const int PIECE_SIZE = 4;  
+
+#endif
