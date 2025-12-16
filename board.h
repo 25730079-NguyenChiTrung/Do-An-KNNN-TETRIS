fac251ce1a42cc19a3530101053cc8368ced362d
nguyenductrung781996-25730080 <nguyenductrung781996@gmail.com>
2025-12-16 21:55:01 +0700

    Update board.h
    Khởi tạo board

--- board.h
+++ board.h
@@ -8,3 +8,29 @@
  * - Xoa hang day (QUAN TRONG!)
  * - Cap nhat diem va toc do
 */
+#ifndef BANGCHOI_H
+#define BANGCHOI_H
+
+
+const int CHIEU_RONG_BANG = 10;
+const int CHIEU_CAO_BANG  = 20;
+const int KICH_THUOC_KHOI = 4;
+
+struct Khoi {
+    int hinh[KICH_THUOC_KHOI][KICH_THUOC_KHOI];
+    int viTriX;
+    int viTriY;
+    int loai;
+    char kyTu;
+};
+
+struct TrangThai {
+    int bang[CHIEU_CAO_BANG][CHIEU_RONG_BANG];
+    Khoi khoiHienTai;
+    int diem;
+    int capDo;
+    int soHangDaXoa;
+    int tocDoCoBan;
+    int tocDoHienTai;
+    bool ketThuc;
+};
