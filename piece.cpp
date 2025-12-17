ad70c8236fff27f3d8f1492731293a8fb993852d
Nguyen Dong Trieu <25730078@ms.uit.edu.vn>
2025-12-17 21:10:42 +0700

    Implement createPiece function

--- piece.cpp
+++ piece.cpp
@@ -16,3 +16,21 @@ const int SHAPES[7][PIECE_SIZE][PIECE_SIZE] = {
     {{0, 0, 0, 0}, {1, 1, 1, 0}, {1, 0, 0, 0}, {0, 0, 0, 0}}};
 
 const char PIECE_SYMBOLS[7] = {'I', 'O', 'T', 'S', 'Z', 'J', 'L'};
+
+Piece createPiece()
+{
+    Piece p;
+    p.type = rand() % 7;
+    p.symbol = PIECE_SYMBOLS[p.type];
+    p.x = BOARD_WIDTH / 2 - 2;
+    p.y = 0;
+
+    for (int i = 0; i < PIECE_SIZE; i++)
+    {
+        for (int j = 0; j < PIECE_SIZE; j++)
+        {
+            p.shape[i][j] = SHAPES[p.type][i][j];
+        }
+    }
+    return p;
+}
