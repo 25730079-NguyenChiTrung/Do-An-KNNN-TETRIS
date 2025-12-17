684dadc8a611f620f3aca798c1ba1d00cd729013
nguyenductrung781996-25730080 <nguyenductrung781996@gmail.com>
2025-12-17 21:23:44 +0700

    khởi tạo
    Khởi tạo

--- board.cpp
+++ board.cpp
@@ -2,3 +2,18 @@
  * THANH VIEN 3: QUAN LY BANG CHOI
  * File: board.cpp
  */
+ #include "bangchoi.h"
+ void khoiTaoBang(TrangThai &tt) {
+    for (int i = 0; i < CHIEU_CAO_BANG; i++) {
+        for (int j = 0; j < CHIEU_RONG_BANG; j++) {
+            tt.bang[i][j] = 0;
+        }
+    }
+
+    tt.diem = 0;
+    tt.capDo = 1;
+    tt.soHangDaXoa = 0;
+    tt.tocDoCoBan = 500;
+    tt.tocDoHienTai = tt.tocDoCoBan;
+    tt.ketThuc = false;
+}
