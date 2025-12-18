6d12ecccd42c9658ee8d1f2f0f161bc638dc9838
nguyenductrung781996-25730080 <nguyenductrung781996@gmail.com>
2025-12-18 22:41:50 +0700

    Update: add constant and struct Khoi and TrangThai

--- board.h
+++ board.h
@@ -1,4 +1,27 @@
 #ifndef BANGCHOI_H
 #define BANGCHOI_H
 
+const int CHIEU_RONG_BANG = 10;
+const int CHIEU_CAO_BANG  = 20;
+const int KICH_THUOC_KHOI = 4;
+
+struct Khoi {
+    int hinh[KICH_THUOC_KHOI][KICH_THUOC_KHOI];
+    int viTriX;
+    int viTriY;
+    int loai;
+    char kyTu;
+};
+
+struct TrangThai {
+    int bang[CHIEU_CAO_BANG][CHIEU_RONG_BANG];
+    Khoi khoiHienTai;
+    int diem;
+    int capDo;
+    int soHangDaXoa;
+    int tocDoCoBan;
+    int tocDoHienTai;
+    bool ketThuc;
+};
+
 #endif
