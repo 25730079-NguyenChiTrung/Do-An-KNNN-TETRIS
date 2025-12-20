4e8b61410bc3383612ffda17f99b9680df19f59c
papercoding22 <25730081@ms.uit.edu.vn>
2025-12-20 11:43:54 +0700

    Sao chep bang vao display

--- display.cpp
+++ display.cpp
@@ -1,7 +1,7 @@
 /*
  * THANH VIEN 4: HIEN THI GIAO DIEN
  * File: display.cpp
-*/
+ */
 
 #include "display.h"
 #include <iostream>
@@ -9,10 +9,30 @@
 
 using namespace std;
 
-void xoaManHinh() {
-	#ifdef _WIN32
-		system("cls");
-	#else
-		system("clear");
-	#endif
+void xoaManHinh()
+{
+#ifdef _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
+}
+
+void ve(GameState &game)
+{
+	xoaManHinh();
+
+	char display[BOARD_HEIGHT][BOARD_WIDTH];
+
+	for (int i = 0; i < BOARD_HEIGHT; ++i)
+	{
+		for (int j = 0; j < BOARD_WIDTH; ++j) {
+			if (game.board[i][j] == 0) {
+				display[i][j] = '.';
+			} else {
+				display[i][j] = '#';
+			}
+		}
+	}
+	
 }
