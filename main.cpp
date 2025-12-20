4f19e79435a7774e784632fb7993be6f1f8954f9
25730079-NguyenChiTrung <25730079@ms.uit.edu.vn>
2025-12-20 11:22:09 +0700

    feat(game): implement basic game loop structure
    - Add game loop function with state initialization
    - Integrate keyboard input checking in continuous loop

--- main.cpp
+++ main.cpp
@@ -9,10 +9,34 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+
+#include "structs.h"
 #include "input.h"
+
+void gameLoop(GameState &game) {
+  // Khoi tao Mock trang thai game
+  game.score = 0;
+  game.level = 1;
+  game.linesCleared = 0;
+  game.baseSpeed = 500;
+  game.currentSpeed = game.baseSpeed;
+  game.gameOver = false;
+
+
+  while (!game.gameOver) {
+    if (keyPressed()) {
+      char key = getKey();
+      std::cout << "Phim da nhan: " << key << std::endl;
+    }
+  }
+}
 
 
 int main() {
+
+  GameState game;
+
+  gameLoop(game);
 
   getKey();
 
