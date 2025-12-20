0c8afee22fa694e814c12a0ead88a1e6741ea6fe
papercoding22 <25730081@ms.uit.edu.vn>
2025-12-20 11:38:44 +0700

    Khai bao cac ham cho chuc nang hien thi game

--- display.h
+++ display.h
@@ -8,3 +8,15 @@
  * - Hien thi thong tin game (score, level, next piece)
  * - Ve man hinh Game Over
 */
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "structs.h"
+
+void xoaManHinh();
+
+void ve(GameState &game);
+
+void veGameOver(GameState &game);
+
+#endif
